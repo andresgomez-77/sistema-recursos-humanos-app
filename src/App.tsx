d44e2d210d633bb7768c710412544efc8b996513
Andrés Gómez <andres071777@hotmail.com>
2026-01-08 21:45:38 -0500

    Adaptando hashRouter para gitPages

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import ListadoEmpleados from "./empleados/ListadoEmpleados";
 import NavBar from "./plantilla/NavBar";
 import AgregarEmpleado from "./empleados/AgregarEmpleado";
 import EditarEmpleado from "./empleados/EditarEmpleado";
-
-
+// usar browser router para usar con el backend cambiar el hashrouter por browserrouter
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <NavBar />
         <Routes>
           <Route path="/" element={<ListadoEmpleados />} />
           <Route path="/agregar" element={<AgregarEmpleado />} />
           <Route path="/editar/:idEmpleado" element={<EditarEmpleado />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
